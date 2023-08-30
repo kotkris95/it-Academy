c3fbca13d4a87a8b9a57aa601cd5cc1ddf094e24
Kristina Kotlyarova <kotkris95@mail.ru>
2023-08-30 22:25:22 +0300

    Solved task_1

---
+++ lesson2/src/Task1.java
@@ -0,0 +1,19 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+
+        System.out.println(x+y);   // в выражениях используются целые числа
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+        double x1 = 5;
+        double y1= 7;
+
+        System.out.println(x1+y1);  // в выражениях используются числа с плавающей точкой
+        System.out.println(x1-y1);
+        System.out.println(x1*y1);
+        System.out.println(x1/y1);
+    }
+}
