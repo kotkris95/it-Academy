60185f86b271071757eab80cb4103c1fa9efd2c6
Kristina Kotlyarova <kotkris95@mail.ru>
2023-08-30 23:17:41 +0300

    Solved task_7

---
+++ lesson2/src/Task7.java
@@ -0,0 +1,12 @@
+public class Task7 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 8;
+        int r = 5;
+        if(2 * r >= Math.sqrt(a * a + b * b)){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
