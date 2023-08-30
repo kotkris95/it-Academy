ee2fc09dd9f8283542066598cb197f140c9b2a86
Kristina Kotlyarova <kotkris95@mail.ru>
2023-08-30 22:45:12 +0300

    Solved task_4

---
+++ lesson2/src/Task4.java
@@ -0,0 +1,10 @@
+public class Task4 {
+    public static void main(String[] args) {
+        double x = 0.25;
+        double y = 0.9;
+
+        System.out.println(10.0 * x + y);
+        System.out.println(y * Math.sqrt(x));
+        System.out.println(Math.pow(x, y) - 1.0);
+    }
+}
