7560ed2eb42464c66cfe8c8645b1fe770c0f83c3
Kristina Kotlyarova <kotkris95@mail.ru>
2023-08-31 21:45:00 +0300

    Solved task_2

--- lesson3/src/Task2.java
+++ lesson3/src/Task2.java
@@ -6,25 +6,25 @@ public class Task2 {
             case (1):
             case (2):
             case (12):
-                System.out.println("Зима");
+                System.out.println("Winter");
                 break;
             case (3):
             case (4):
             case (5):
-                System.out.println("Весна");
+                System.out.println("Spring");
                 break;
             case (6):
             case (7):
             case (8):
-                System.out.println("Лето");
+                System.out.println("Summer");
                 break;
             case (9):
             case (10):
             case (11):
-                System.out.println("Осень");
+                System.out.println("Autumn");
                 break;
             default:
-                System.out.println("Введите число от 1 до 12");
+                System.out.println("Enter a number from 1 to 12");
         }
     }
 }
