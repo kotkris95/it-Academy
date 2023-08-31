7c983eb0f9559a4f752ce59c185e396f999d03e0
Kristina Kotlyarova <kotkris95@mail.ru>
2023-08-31 21:28:17 +0300

    Solved task_1

---
+++ lesson3/src/Task1.java
@@ -0,0 +1,14 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 8;
+        int z = 6;
+
+        if(x > z){
+            System.out.println((x+y)/2);
+        } else {
+            System.out.println(z);
+        }
+
+    }
+}
