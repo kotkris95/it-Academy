0a48b0b8c34d073b670af5d29ef562108ee2d999
Kristina Kotlyarova <kotkris95@mail.ru>
2023-09-03 22:32:19 +0300

    Changes task_7

--- lesson2/src/Task7.java
+++ lesson2/src/Task7.java
@@ -3,10 +3,7 @@ public class Task7 {
         int a = 6;
         int b = 8;
         int r = 5;
-        if(2 * r >= Math.sqrt(a * a + b * b)){
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }
+        System.out.println(2 * r >= Math.sqrt(a * a + b * b));
+
     }
 }
