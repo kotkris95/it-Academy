7e9df916ae342ca69dfd7cb0be7cb0f8ab5150a8
Kristina Kotlyarova <kotkris95@mail.ru>
2023-09-03 22:30:49 +0300

    Changes task_6

--- lesson2/src/Task6.java
+++ lesson2/src/Task6.java
@@ -11,26 +11,10 @@ public class Task6 {
         int i8 = 8;
         int i9 = 9;
 
-        int averageInt = (i0 + i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9) /10;
-        System.out.println(averageInt);
+        int sum = i0 + i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9;
+        System.out.println(sum/10);
+        System.out.println(sum/10.0);
 
-        double d0 = 0;
-        double d1 = 1;
-        double d2 = 2;
-        double d3 = 3;
-        double d4 = 4;
-        double d5 = 5;
-        double d6 = 6;
-        double d7 = 7;
-        double d8 = 8;
-        double d9 = 9;
 
-        double averageDouble = (d0 + d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9) /10;
-        System.out.println(averageDouble);
-
-        /*
-        в данном примере корректнее использовать double, т.к. при использовании int,
-        результат не округляется, а просто отбрасывается часть после точки
-         */
     }
 }
