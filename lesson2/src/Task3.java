62377b255742d81d7a2b5cd7b0d8e4c3394275b3
Kristina Kotlyarova <kotkris95@mail.ru>
2023-09-03 22:27:23 +0300

    Changes task_3

--- lesson2/src/Task3.java
+++ lesson2/src/Task3.java
@@ -2,7 +2,7 @@ public class Task3 {
     public static void main(String[] args) {
         int x = 1234;
         int lastTwoNumbers = x % 100;
-        int firstTwoNumbers = (x - lastTwoNumbers) / 100;
+        int firstTwoNumbers = x/ 100;
         System.out.println(firstTwoNumbers);
         System.out.println(lastTwoNumbers);
     }
