47c6764f2c0ebacb5b58b91c6ed40aa6e94669a9
Kristina Kotlyarova <kotkris95@mail.ru>
2023-09-17 13:54:46 +0300

    Changes

--- lesson5/src/Task9.java
+++ lesson5/src/Task9.java
@@ -4,22 +4,28 @@ import java.util.Arrays;
 public class Task9 {
     public static void main(String[] args) {
         int[] array = new int[]{6, 8, 6, 1, 10, 9, 8, 6, 9, 11, 12, 2, 3};
+        System.out.println(Arrays.toString(removeDuplicates(array)));
+    }
+    public static int[] removeDuplicates(int[] arr) {
+        int end = arr.length;
 
-            System.out.println("Исходный массив: " + Arrays.toString(array));
-            int[] arrayWithoutDuplicates = removeDuplicates(array);
-            System.out.println("Массив без повторяющихся элементов: "
-                    + Arrays.toString(arrayWithoutDuplicates)
-            );
-        }
-
-        public static int[] removeDuplicates(int[] array) {
-            Arrays.sort(array);
-            int k = 0;
-            for (int i = 0; i < array.length; i++) {
-                if (i == 0 || array[i] != array[i - 1]) {
-                    array[k++] = array[i];
+        for (int i = 0; i < end; i++) {
+            for (int j = i + 1; j < end; j++) {
+                if (arr[i] == arr[j]) {
+                    int shiftLeft = j;
+                    for (int k = j+1; k < end; k++, shiftLeft++) {
+                        arr[shiftLeft] = arr[k];
+                    }
+                    end--;
+                    j--;
                 }
             }
-            return Arrays.copyOf(array, k);
         }
+
+        int[] whitelist = new int[end];
+        for(int i = 0; i < end; i++){
+            whitelist[i] = arr[i];
+        }
+        return whitelist;
+    }
 }
