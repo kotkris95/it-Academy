d43af83efe0f3b8fa545e432920bcccb6bc50ee3
Kristina Kotlyarova <kotkris95@mail.ru>
2023-09-17 14:33:52 +0300

    Changes

--- lesson5/src/Task10.java
+++ lesson5/src/Task10.java
@@ -2,64 +2,54 @@ import java.util.Arrays;
 
 public class Task10 {
     public static void main(String[] args) {
-         char [][] field = new char[3][3];
-         char selectLiteral1;
-         char selectLiteral2;
-         int moveI;
-         int moveJ;
+        char[][] field = new char[3][3];
+        char selectLiteral1;
+        char selectLiteral2;
+        int moveI;
+        int moveJ;
 
         for (char[] chars : field) {
             Arrays.fill(chars, '\'');
         }
 
-        System.out.println("Игрок №1, выберите, чем будете играть: Х или О.");
         selectLiteral1 = 'X';
-        System.out.println("Вы выбрали: " + selectLiteral1);
         selectLiteral2 = 'O';
-        System.out.println("Для игрока №2: " + selectLiteral2);
+        boolean isSet;
+        char currentLiteral = selectLiteral1;
 
-         while(true) {
-            System.out.println("Ход игрока №1! Поставьте " + selectLiteral1 + " на выбранную позицию.");
-            moveI = (int) (Math.random()*3);
-            moveJ =(int) (Math.random()*3);
-            field = makeMove(field, moveI, moveJ, selectLiteral1);
-            if (isGameOver(field, selectLiteral1)){
-                System.out.println("Победил игрок №1!");
+        while (true) {
+            do {
+                System.out.println("Поставьте " + currentLiteral + " на свободную позицию.");
+                moveI = (int) (Math.random() * 3);
+                moveJ = (int) (Math.random() * 3);
+                isSet = makeMove(field, moveI, moveJ, currentLiteral);
+            } while (!isSet);
+            if (isGameOver(field, currentLiteral)) {
+                System.out.println("Победил " + currentLiteral);
                 break;
             }
-            if(isFriendshipWon(field)){
+            if (isFriendshipWon(field)) {
                 System.out.println("Победила дружба!");
                 break;
             }
-            System.out.println("Ход игрока №2! Поставьте " + selectLiteral2 + " на выбранную позицию.");
-             moveI = (int) (Math.random()*3);
-             moveJ =(int) (Math.random()*3);
-            field = makeMove(field, moveI, moveJ, selectLiteral2);
-            if (isGameOver(field, selectLiteral2)){
-                System.out.println("Победил игрок №2!");
-                break;
-            }
-            if(isFriendshipWon(field)){
-                System.out.println("Победила дружба!");
-                break;
-            }
+            currentLiteral = currentLiteral == selectLiteral1 ? selectLiteral2 : selectLiteral1;
         }
     }
-    static boolean isEmpty(char [][] array, int i, int j){
+
+    static boolean isEmpty(char[][] array, int i, int j) {
         return array[i][j] == '\'';
     }
 
-    static char[][] makeMove(char [][] array, int i, int j, char lit){
-        if(isEmpty(array, i, j)) {
+    static boolean makeMove(char[][] array, int i, int j, char lit) {
+        if (isEmpty(array, i, j)) {
             array[i][j] = lit;
-        }else{
-            System.out.println("Выбранная позиция занята!");
+            print(array);
+            return true;
         }
-        print(array);
-        return array;
+        return false;
     }
 
-    static void print (char[][] array) {
+    static void print(char[][] array) {
         for (char[] chars : array) {
             for (char aChar : chars) {
                 System.out.print(aChar + " ");
@@ -68,20 +58,19 @@ public class Task10 {
         }
     }
 
-    static boolean isGameOver(char[][] array, char lit){
+    static boolean isGameOver(char[][] array, char lit) {
         boolean isGameOver = false;
-        for(int i = 0; i<3; i++){
-            if((array[i][0] == lit && array[i][1] == lit && array[i][2] == lit)
-                    || (array[0][i] == lit && array[1][i] == lit && array[2][i] == lit)
-            ) isGameOver = true;
-            if(array[0][0] == lit && array[1][1] == lit && array[2][2] == lit
-                     || array[0][2] == lit && array[1][1] == lit && array[2][0] == lit
-            ) isGameOver = true;
-        }
+        if ((array[0][0] == lit && array[0][1] == lit && array[0][2] == lit)
+                || (array[0][0] == lit && array[1][0] == lit && array[2][0] == lit)
+        ) isGameOver = true;
+        if (array[0][0] == lit && array[1][1] == lit && array[2][2] == lit
+                || array[0][2] == lit && array[1][1] == lit && array[2][0] == lit
+        ) isGameOver = true;
+
         return isGameOver;
     }
 
-    static boolean isFriendshipWon(char[][] array){
+    static boolean isFriendshipWon(char[][] array) {
         for (char[] chars : array) {
             for (char aChar : chars) {
                 if (aChar == '\'')
@@ -90,5 +79,4 @@ public class Task10 {
         }
         return true;
     }
-
 }
