7f77c98dfef2883e383ea38af06e88d5385bfc29
Kristina Kotlyarova <kotkris95@mail.ru>
2023-09-17 12:17:44 +0300

    Changes

--- lesson5/src/Task8.java
+++ lesson5/src/Task8.java
@@ -1,47 +1,43 @@
 public class Task8 {
     public static void main(String[] args) {
-        int [][] array = new int [7][7];
-        int dioganal = 0;
-        int inversDioganal = array.length-1;
+        int[][] array = new int[7][7];
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                if(j == dioganal || j == inversDioganal){
+                if (j == i || j == array.length - i - 1) {
                     array[i][j] = 0;
-                }else if(j>dioganal && j < inversDioganal || j<dioganal && j>inversDioganal){
-                    array[i][j] = (int) ((Math.random()+0.1) * 10);
-                }else {
-                    array[i][j] = (int) ((Math.random()+0.1) * -10);
+                } else if (j > i && j < array.length - i - 1 || j < i && j > array.length - i - 1) {
+                    array[i][j] = (int) ((Math.random() + 0.1) * 10);
+                } else {
+                    array[i][j] = (int) ((Math.random() + 0.1) * -10);
                 }
             }
-            dioganal++;
-            inversDioganal--;
         }
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                sum+=array[i][j];
+                sum += array[i][j];
                 System.out.printf("%3d", array[i][j]);
             }
             System.out.println();
         }
         System.out.println("Сумма всех элементов массива: " + sum);
         System.out.println("Среднее арифметическое всех элементов больше " + sum + ": "
-                + getSum(array, sum)
+                + getAverage(array, sum)
         );
-
     }
-    static double getSum (int[][] array, int value){
+
+    static double getAverage(int[][] array, int value) {
         double sum = 0;
         int count = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                if(array[i][j]>value){
-                    sum+=array[i][j];
+                if (array[i][j] > value) {
+                    sum += array[i][j];
                     count++;
+                }
             }
         }
-    }
-        return sum/count;
+        return sum / count;
     }
 }
