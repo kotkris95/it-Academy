591bfe0cef0bd90a4d5aedb5e21b250c878e26a1
Kristina Kotlyarova <kotkris95@mail.ru>
2023-09-17 11:45:14 +0300

    Changes

--- lesson5/src/Task3.java
+++ lesson5/src/Task3.java
@@ -2,49 +2,18 @@ public class Task3 {
     public static void main(String[] args) {
         int dayOfMonth =8;
         int month = 10;
-        int [][] year = new int[12][];
-        int [][] initYear = initialization(year);
-        int dayOfYear = getDayOfYear(initYear, month, dayOfMonth);
-        System.out.println("Порядковый номер дня года: " + dayOfYear);
-    }
-    static int getDayOfYear(int[][] year, int month, int dayOfMonth) {
+        int[] year = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int dayOfYear = 0;
-        if(month > 0 && month < 13 && dayOfMonth>0 && dayOfMonth < 32) {
-            for (int i = 0; i < month - 1; i++) {
-                for (int j = 0; j < year[i].length; j++) {
-                    dayOfYear++;
-                }
+
+        for (int i = 0; i <= month-2; i++) {
+            if(month > year.length){
+                System.out.println("Введены некорректные данные!");
             }
-        }else{
-            System.out.println("Введены некорректные данные!");
-            return 0;
+            dayOfYear += year[i];
         }
-        return dayOfYear+=dayOfMonth;
-    }
+        dayOfYear += dayOfMonth;
+        System.out.println("Порядковый номер дня года: " + dayOfYear);
 
-    static int [][] initialization(int [][] year) {
-        for (int i = 0; i < year.length; i++) {
-            year[i] = getDaysOfMonth(i);
-        }
-        return year;
-    }
 
-    static int[] getDaysOfMonth(int month) {
-        if(month > 11 || month < 0){
-            System.out.println("Переменная month некорректно инициализирована!");
-        }
-        int[] daysOfMonth = new int[0];
-        if(month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11){
-            daysOfMonth = new int[31];
-        } else if(month==1){
-            daysOfMonth = new int[28];
-        } else {
-            daysOfMonth = new int[30];
-        }
-
-        for(int i = 0; i < daysOfMonth.length; i++){
-            daysOfMonth[i] = i+1;
-        }
-        return daysOfMonth;
     }
 }
