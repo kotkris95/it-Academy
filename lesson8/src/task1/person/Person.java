11cf5e219da28bc844d8b7b1ab99fd0eabf04d69
Kristina Kotlyarova <kotkris95@mail.ru>
2023-10-11 21:58:01 +0300

    Changes

--- lesson8/src/task1/person/Person.java
+++ lesson8/src/task1/person/Person.java
@@ -6,11 +6,16 @@ public class Person {
     private String gender;
     private double weight;
 
+    public static final String MALE = "мужской";
+    public static final String FEMALE = "женский";
+
     public Person(String name, int age, String gender, double weight) {
-        setName(name);
-        setAge(age);
-        setGender(gender);
-        setWeight(weight);
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        if(gender.equals(MALE) || gender.equals(FEMALE)){
+            this.gender = gender;
+        }
     }
 
     public String getName() {
@@ -32,19 +37,17 @@ public class Person {
     public void setName(String name) {
         if (name.length() != 0) {
             this.name = name;
-        } else System.out.println("Имя введено некорректно");
+        } else {
+            System.out.println("Имя введено некорректно");
+        }
     }
 
     public void setAge(int age) {
         if (age > 0) {
             this.age = age;
-        } else System.out.println("Возраст введен некорректно");
-    }
-
-    public void setGender(String gender) {
-        if (gender.equals("female") || gender.equals("male")) {
-            this.gender = gender;
-        } else System.out.println("Пол введен некорректно");
+        } else {
+            System.out.println("Возраст введен некорректно");
+        }
     }
 
     public void setWeight(double weight) {
