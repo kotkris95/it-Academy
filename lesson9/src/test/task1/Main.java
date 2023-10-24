8879dbcbe9962fe6dfa194a7a2e6a627a13d31a8
Kristina Kotlyarova <kotkris95@mail.ru>
2023-10-24 21:02:30 +0300

    Changes

--- lesson9/src/test/task1/Main.java
+++ lesson9/src/test/task1/Main.java
@@ -3,12 +3,12 @@ package test.task1;
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal();
-        String str = "Привет";
+        String str = "РџСЂРёРІРµС‚";
         Integer i = 88;
         Generic<String, Animal, Integer> generic = new Generic<>(str, animal, i);
-        System.out.println("Значение переменной T: " + generic.getT());
-        System.out.println("Значение переменной V: " + generic.getV());
-        System.out.println("Значение переменной K: " + generic.getK());
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ РїРµСЂРµРјРµРЅРЅРѕР№ T: " + generic.getT());
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ РїРµСЂРµРјРµРЅРЅРѕР№ V: " + generic.getV());
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ РїРµСЂРµРјРµРЅРЅРѕР№ K: " + generic.getK());
 
     }
 }
